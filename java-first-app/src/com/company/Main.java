b17ca9d4542d441851c18bbcecbae3f87f94e50a
barbodnavabi <barbodnvb@gmail.com>
2021-05-03 11:19:31 +0430

    fix

---
+++ java-first-app/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("\t\"hello world\"\n\tthis \\is line two");
+	System.out.println(12);
+    }
+/*
+* for new line we write  /n
+* for a tab on text we write /t
+* for a quote we write /""/
+* for a backslash we write //
+* */
+}
